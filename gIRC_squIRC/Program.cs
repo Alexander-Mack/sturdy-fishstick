dbff17a178d84ada922b6d587227b9070969a44a
Alex <agarymack@gmail.com>
2023-05-15 15:49:18 -0600

    Updated IRC server and Created Client

--- gIRC_squIRC/Program.cs
+++ gIRC_squIRC/Program.cs
@@ -12,64 +12,92 @@ namespace gIRC_squIRC
         private static string path = "";
         static void Main(string[] args)
         {
+            // boot server
             Console.WriteLine("Server starting up ...");
             DateTime current = DateTime.Now;
+            // set the local log file to the current day.
             path = current.Day + "-" + current.Month + "-" + current.Year + ".txt";
+            Console.WriteLine("Today is: {0}-{1}-{2}", current.Day, current.Month, current.Year);
+            // if the log file does not exist create a new one with a header
             if (!File.Exists(path))
             {
                 File.Create(path);
                 File.WriteAllText(path, "[File Head]");
             }
+            // launch server
             StartServer(path);
         }
 
         private static void StartServer(string logs)
         {
-            TcpListener server;
-            Int32 port = 11000;
-            IPAddress local = IPAddress.Parse("68.148.70.170");
-            server = new TcpListener(local, port);
-            server.Start();
-            Console.WriteLine("Server is started ...");
-            while (true)
+            try
             {
-                TcpClient client = server.AcceptTcpClient();
-                ThreadPool.QueueUserWorkItem(ThreadProc, client);
+                TcpListener server;
+                Int32 port = 11000;
+                // Set local address, 10.0.0.177 for now
+                IPAddress local = IPAddress.Parse("10.0.0.177");
+                server = new TcpListener(local, port);
+                // launch server on ip and port
+                server.Start();
+                Console.WriteLine("Server is started ...");
+                // loop while running
+                while (true)
+                {
+                    // if a new client is received
+                    TcpClient client = server.AcceptTcpClient();
+                    // create a new thread to handle it.
+                    ThreadPool.QueueUserWorkItem(ThreadProc, client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
             }
 
         }
 
-        private static void ThreadProc(object obj)
+        private static void ThreadProc(object? obj)
         {
-            var client = (TcpClient)obj;
+            var client = (TcpClient)obj!;
+            // get stream of client
+            NetworkStream stream = client.GetStream();
             try
             {
-                NetworkStream stream = client.GetStream();
                 String data = "";
                 Byte[] bytes = new Byte[256];
                 int i;
-                while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+                // while client continues to message
+                while (true)
                 {
+                    // receive first part of message as user information
+                    i = stream.Read(bytes, 0, bytes.Length);
+                    string info = Encoding.ASCII.GetString(bytes, 0, i);
+                    // receive second part of message as message contents
+                    i = stream.Read(bytes, 0, bytes.Length);
                     data = Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
+                    // write to file or output
+                    Console.WriteLine("Received: {0}\nFrom: {1}", data, info);
                     // TODO: process data 
                     // Use semaphores to prevent race conditions
                     // Lock(semaphore);
-                    data = data.ToUpper(); 
+                    data = info + ": " + data;
                     // WriteToLog(path, log);
                     // Unlock(semaphore);
                     byte[] msg = Encoding.ASCII.GetBytes(data);
+                    // return updated log? maybe some other trigger here
                     stream.Write(msg, 0, msg.Length);
                     Console.WriteLine("Sent: {0}", data);
-                    stream.Close();
-                    client.Close();
                 }
-            }catch(SocketException e)
+
+            }
+            catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
+
             }
             finally
             {
+                stream.Close();
                 client.Close();
             }
 
---
+++ gsIRC_Client/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace gsIRC_Client
+{
+    class gsIRC_Client
+    {
+        static void Main(string[] args)
+        {
+            // args will be username
+            // if not given ask for one
+            // try to connect to server
+            // wait for user to send a message
+            // return confirmation message
+            // send logs to user?
+            string user;
+            // Get username from command line argument or ask for a username
+            // TODO: Needs validator
+            if (args.Length == 0)
+            {
+                Console.Write("Please enter your screen name: ");
+                user = Console.ReadLine();
+            }
+            else
+            {
+                user = args[0];
+            }
+            // Attempt to connect to the server
+            try
+            {
+                // Address may need to change from here
+                IPAddress server_address = IPAddress.Parse("10.0.0.177");
+                Int32 server_port = 11000;
+                IPEndPoint ep = new IPEndPoint(server_address, server_port);
+                TcpClient server = new TcpClient();
+                server.Connect(ep);
+                NetworkStream stream = server.GetStream();
+                Thread receiver = new Thread(IncomingHandler);
+                receiver.Start(stream);
+                // go to message handler
+                OutgoingHandler(user, stream);
+                
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+        private static void OutgoingHandler(string user, NetworkStream stream)
+        {
+            string message = "";
+            int i;
+            // open the message stream
+            
+            // continue until user signs off
+            while (true)
+            {
+                try
+                {
+                    // timestamp and wait for input
+                    DateTime current = DateTime.Now;
+                    // format timestamp and user info
+                    String info = "[" + current.ToString("HH:mm:ss") + "] " + user;
+                    // Console.Write("{0}: ", info);
+                    // receive message contents from user
+                    message = Console.ReadLine();
+                    // send first part of message as user info and timestamp
+                    byte[] sender = Encoding.ASCII.GetBytes(info);
+                    stream.Write(sender, 0, sender.Length);
+                    // send second part of message as message contents
+                    byte[] msg = Encoding.ASCII.GetBytes(message);
+                    stream.Write(msg, 0, msg.Length);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e);
+                }
+            }
+        }
+
+        private static void IncomingHandler(object obj)
+        {
+            var stream = (NetworkStream) obj;
+            byte[] bytes = new byte[256];
+            int i;
+            string data = "";
+            // continue until user signs off
+            while(true)
+            {
+                try
+                {
+                    // receive new message from other users
+                    i = stream.Read(bytes, 0, bytes.Length);
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    // write to screen, will need mutual exclusion
+                    Console.WriteLine("{0}: ", data);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e);
+                }
+            }
+        }
+    }
+}
